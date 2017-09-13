fc073779c2b5aea7c20d0bd22d19ced89ddb42a2
PhoenixAzrael <christian.brueckner@live.com>
2017-09-13 17:47:34 +0200

    Create LANBASE_NW_Reconnect.bat

---
+++ LANBASE_NW_Reconnect.bat
@@ -0,0 +1,15 @@
+net use g: /d
+net use h: /d
+net use m: /d
+net use r: /d
+net use t: /d
+net use u: /d
+net use z: /d
+
+net use g: \\arrakis\games
+net use h: \\arrakis\christian
+net use m: \\arrakis\musix
+net use r: \\arrakis\root
+net use t: \\arrakis\abcTeam
+net use u: \\arrakis\upload
+net use z: \\arrakis\data

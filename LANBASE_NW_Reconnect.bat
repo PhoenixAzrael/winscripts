8786e48707ac422e83805b73f9be2dab8e6a82ae
PhoenixAzrael <christian.brueckner@live.com>
2018-09-14 09:08:28 +0200

    Update LANBASE_NW_Reconnect.bat

--- LANBASE_NW_Reconnect.bat
+++ LANBASE_NW_Reconnect.bat
@@ -13,3 +13,6 @@ net use r: \\arrakis\root /persistent:yes
 net use t: \\arrakis\abcTeam /persistent:yes
 net use u: \\arrakis\upload /persistent:yes
 net use z: \\arrakis\data /persistent:yes
+
+
+New-PSDrive -Name <drive letter> -PSProvider FileSystem -Root "\\ComputerName\ShareName" -Credential "UserName" -Persist

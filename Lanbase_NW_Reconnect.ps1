325443a7ef32f70daa8068aa9abaf7436992bafd
PhoenixAzrael <christian.brueckner@live.com>
2019-06-02 10:50:09 +0200

    Update H to Christian

--- Lanbase_NW_Reconnect.ps1
+++ Lanbase_NW_Reconnect.ps1
@@ -1,5 +1,6 @@
+Get-PSDrive G, H, M, R, T, U, Z | Remove-PSDrive
 New-PSDrive -Name "G" -PSProvider FileSystem -Root "\\arrakis\games" -Persist
-New-PSDrive -Name "H" -PSProvider FileSystem -Root "\\arrakis\root" -Persist
+New-PSDrive -Name "H" -PSProvider FileSystem -Root "\\arrakis\christian" -Persist
 New-PSDrive -Name "M" -PSProvider Filesystem -Root "\\arrakis\musix" -Persist
 New-PSDrive -Name "R" -PSProvider Filesystem -Root "\\arrakis\root" -Persist
 New-PSDrive -Name "T" -PSProvider Filesystem -Root "\\arrakis\abcTeam" -Persist

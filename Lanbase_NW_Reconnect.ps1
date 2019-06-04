ebc39164f7373f835da86e998ee43869afcb7860
PhoenixAzrael <christian.brueckner@live.com>
2019-06-04 19:46:02 +0200

    Add "-Scope Global"
    Connection gets dropped because of garbage collection of powershell. 
    Setting forces to retain network drives after garbage collection.

--- Lanbase_NW_Reconnect.ps1
+++ Lanbase_NW_Reconnect.ps1
@@ -1,8 +1,8 @@
 Get-PSDrive G, H, M, R, T, U, Z | Remove-PSDrive
-New-PSDrive -Name "G" -PSProvider FileSystem -Root "\\arrakis\games" -Persist
-New-PSDrive -Name "H" -PSProvider FileSystem -Root "\\arrakis\christian" -Persist
-New-PSDrive -Name "M" -PSProvider Filesystem -Root "\\arrakis\musix" -Persist
-New-PSDrive -Name "R" -PSProvider Filesystem -Root "\\arrakis\root" -Persist
-New-PSDrive -Name "T" -PSProvider Filesystem -Root "\\arrakis\abcTeam" -Persist
-New-PSDrive -Name "U" -PSProvider Filesystem -Root "\\arrakis\upload" -Persist
-New-PSDrive -Name "Z" -PSProvider Filesystem -Root "\\arrakis\data" -Persist
+New-PSDrive -Name "G" -PSProvider FileSystem -Root "\\arrakis\games" -Persist -Scope Global
+New-PSDrive -Name "H" -PSProvider FileSystem -Root "\\arrakis\christian" -Persist -Scope Global
+New-PSDrive -Name "M" -PSProvider Filesystem -Root "\\arrakis\musix" -Persist -Scope Global
+New-PSDrive -Name "R" -PSProvider Filesystem -Root "\\arrakis\root" -Persist -Scope Global
+New-PSDrive -Name "T" -PSProvider Filesystem -Root "\\arrakis\abcTeam" -Persist -Scope Global
+New-PSDrive -Name "U" -PSProvider Filesystem -Root "\\arrakis\upload" -Persist -Scope Global
+New-PSDrive -Name "Z" -PSProvider Filesystem -Root "\\arrakis\data" -Persist -Scope Global

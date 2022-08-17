98975e16d230399e58a4beb3be4d0875f6ea0bf4
PhoenixAzrael <christian.brueckner@live.com>
2022-08-17 13:21:23 +0200

    Update LANBASE_NW_Reconnect.bat

--- LANBASE_NW_Reconnect.bat
+++ LANBASE_NW_Reconnect.bat
@@ -4,6 +4,9 @@ net use m: /d
 net use r: /d
 net use t: /d
 net use u: /d
+net use w: /d
+net use x: /d
+net use y: /d
 net use z: /d
 
 net use g: \\arrakis\games /persistent:yes
@@ -12,4 +15,7 @@ net use m: \\arrakis\musix /persistent:yes
 net use r: \\arrakis\root /persistent:yes
 net use t: \\arrakis\abcTeam /persistent:yes
 net use u: \\arrakis\upload /persistent:yes
+net use w: \\arrakis\temp /persistent:yes
+net use x: \\arrakis\ext1 /persistent:yes
+net use y: \\arrakis\ext2 /persistent:yes
 net use z: \\arrakis\data /persistent:yes
